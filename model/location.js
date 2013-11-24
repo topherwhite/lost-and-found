2267e21eabc1cb48596fd2640448673795f964d0
JD <delaplagnejd@gmail.com>
2013-11-24 14:09:08 -0500

    fix crash

--- model/location.js
+++ model/location.js
@@ -2,7 +2,7 @@ exports.defineModel = function(sequelize, DataTypes) {
   return sequelize.define(
     "Location", {
 		type: { type: DataTypes.ENUM, allowNull: false, unique: true, validate: {}, values: ['lost', 'found']}	
-    },
+    }
     {
       // column naming customization
       instanceMethods: {
