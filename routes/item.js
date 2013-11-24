7cd09518a8eb763ab72e49d21134ab5886ae036b
topherwhite <topherwhite@gmail.com>
2013-11-24 16:13:34 -0500

    more api stuff

--- routes/item.js
+++ routes/item.js
@@ -14,8 +14,19 @@ exports.create = function(req, res) {
     email: req.body.email.toLowerCase()
   }).success(function(_Person){
     if (typeof req.body.phone !== "undefined") { _Person.phone = req.body.phone; _Person.save(); }
-    
-    res.send(_Person);
+    _Person.addItem({
+      type: req.body.type,
+      title: req.body.title,
+      imagelink: req.body.imagelink,
+      description: req.body.description
+    }).success(function(_Item){
+
+
+      res.send(_Item);
+
+    }).error(function(e){
+      console.error(e); res.send({},500);
+    });
   }).error(function(e){
     console.error(e); res.send({},500);
   });
---
+++ routes/person.js
@@ -0,0 +1,11 @@
+
+var Model = global.Model;
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+
+exports.create = function(req, res) {
+  res.send("respond with a resource");
+};
